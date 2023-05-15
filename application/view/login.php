ead5417ffee19523360015cf3ec32c60e9a1ccd6
ByungjooPark <exit>
2023-05-15 19:02:35 +0900

    0515 메인페이지 변경

--- application/view/login.php
+++ application/view/login.php
@@ -4,6 +4,8 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="/application/view/css/common.css">
+	<script src="/application/view/js/common.js"></script>
 	<title>Login</title>
 </head>
 <body>
